d9a1de209bac2d6c3c0a9467e5115249ebc3bb89
Spotexx <tsanderson.023@gmail.com>
2022-03-12 14:41:30 -0500

    delete post from user

--- api/postRoutes.js
+++ api/postRoutes.js
@@ -5,7 +5,7 @@ const router = require('express').Router();
 /*
     - New post for existing user
     - POST /posts/:userid
-    - body: { "post": "string"}
+    - body: { title: "string", body: "string" }
 */
 router.post('/:userId', async (req, res) => {
     User.findOneAndUpdate({ _id: req.params.userId }, { $push: { posts: { title: req.body.title, body: req.body.body, replies: [] } } }, { new: true }, (err, user) => {
@@ -16,5 +16,19 @@ router.post('/:userId', async (req, res) => {
         res.json(user);
     });
 });
+
+/*
+    - Delete a post
+    - DELETE /posts/:userid/:postid
+*/
+router.delete('/:userId/:postId', async (req, res) => {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $pull: { posts: { _id: req.params.postId } } }, { new: true }, (err, user) => {
+        if (err) {
+            res.send(err);
+            return;
+        }
+        res.json(user);
+    });
+})
 
 module.exports = router;
