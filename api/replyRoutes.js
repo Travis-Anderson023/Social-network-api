f4a915586cbe742c37adedb4199bbda46ae62db0
Spotexx <tsanderson.023@gmail.com>
2022-03-12 14:58:38 -0500

    post reply

---
+++ api/replyRoutes.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const User = require('../models/User');
+const router = require('express').Router();
+
+/*
+    - Create reply to post
+    - POST /replies/:postOwnerId/:postId
+    - body: { reply: "string" }
+*/
+router.post('/:postOwnerId/:postId', async (req, res) => {
+    User.findOneAndUpdate({ _id: req.params.postOwnerId, "posts._id": req.params.postId }, { $push: { "posts.$.replies": { body: req.body.body, ownerId: req.body.ownerId } } }, { new: true }, (err, user) => {
+        if (err) {
+            res.send(err);
+            return;
+        }
+        res.json(user);
+    });
+});
+
+
+
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,7 +1,3 @@
-//get all, create delete user
-//view connections, create delete friends (add remove)
-
-//get all, create, delete, update thoughts
 //get all, create, delete reactions
 
 const User = require('./User');
